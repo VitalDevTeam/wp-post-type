0ff9c02c7c86809ddf1efdc2ba25998e6582b4af
Adam Walter <adam@adamwalter.com>
2014-07-14 17:36:26 -0400

    Simplify and revise code

--- custom-post-type-boilerplate.php
+++ custom-post-type-boilerplate.php
@@ -9,106 +9,111 @@
     License: GPL2
 */
 
-function widget_register() {
-    register_post_type( 'widget', array(
-        'hierarchical'      => false,
-        'public'            => true,
-        'show_in_nav_menus' => true,
-        'show_ui'           => true,
-        'menu_icon'         => 'dashicons-star-filled', // Dashicons CSS class name - http://melchoyce.github.io/dashicons/
-        'menu_position'     => 10,
-        'supports'          => array( 'title', 'editor' ),
-        'has_archive'       => true,
-        'query_var'         => true,
-        'rewrite'           => true,
-        'labels'            => array(
-            'name'                => __( 'Widgets', 'YOUR-TEXTDOMAIN' ),
-            'singular_name'       => __( 'Widget', 'YOUR-TEXTDOMAIN' ),
-            'add_new'             => __( 'Add new Widget', 'YOUR-TEXTDOMAIN' ),
-            'all_items'           => __( 'Widgets', 'YOUR-TEXTDOMAIN' ),
-            'add_new_item'        => __( 'Add new Widget', 'YOUR-TEXTDOMAIN' ),
-            'edit_item'           => __( 'Edit Widget', 'YOUR-TEXTDOMAIN' ),
-            'new_item'            => __( 'New Widget', 'YOUR-TEXTDOMAIN' ),
-            'view_item'           => __( 'View Widget', 'YOUR-TEXTDOMAIN' ),
-            'search_items'        => __( 'Search Widgets', 'YOUR-TEXTDOMAIN' ),
-            'not_found'           => __( 'No Widgets found', 'YOUR-TEXTDOMAIN' ),
-            'not_found_in_trash'  => __( 'No Widgets found in trash', 'YOUR-TEXTDOMAIN' ),
-            'parent_item_colon'   => __( 'Parent Widget', 'YOUR-TEXTDOMAIN' ),
-            'menu_name'           => __( 'Widgets', 'YOUR-TEXTDOMAIN' ),
-        ),
-    ) );
-}
-add_action( 'init', 'widget_register' );
+/*  ==========================================================================
+     INIT
+    ==========================================================================  */
+
+    function widget_init() {
+        $labels = array(
+            'name'               => 'Widgets', 'post type general name',
+            'singular_name'      => 'Widget', 'post type singular name',
+            'menu_name'          => 'Widgets', 'admin menu',
+            'name_admin_bar'     => 'Widget', 'add new on admin bar',
+            'add_new'            => 'Add New', 'Widget',
+            'add_new_item'       => 'Add New Widget',
+            'new_item'           => 'New Widget',
+            'edit_item'          => 'Edit Widget',
+            'view_item'          => 'View Widget',
+            'all_items'          => 'All Widgets',
+            'search_items'       => 'Search Widgets',
+            'parent_item_colon'  => 'Parent Widgets:',
+            'not_found'          => 'No Widget found.',
+            'not_found_in_trash' => 'No Widget found in Trash.'
+        );
+        $args = array(
+            'labels'             => $labels,
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'menu_icon'          => 'dashicons-star-filled', // Dashicons CSS class name - http://melchoyce.github.io/dashicons/
+            'menu_position'      => 20,
+            'show_in_menu'       => true,
+            'query_var'          => true,
+            // 'rewrite'            => array( 'slug' => 'widget' ),
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'hierarchical'       => false,
+            'menu_position'      => null,
+            'supports'           => array( 'title', 'editor' )
+        );
+        register_post_type( 'widget', $args );
+    }
+    add_action( 'init', 'widget_init' );
+
+
+    /*  ==========================================================================
+         UPDATE MESSAGES
+        ==========================================================================  */
+
+    function widget_updated_messages( $messages ) {
+        $post             = get_post();
+        $post_type        = get_post_type( $post );
+        $post_type_object = get_post_type_object( $post_type );
+        $messages['widget'] = array(
+            0  => '',
+            1  => 'Widget updated.',
+            2  => 'Custom field updated.',
+            3  => 'Custom field deleted.',
+            4  => 'Widget updated.',
+            5  => isset( $_GET['revision'] ) ? sprintf( 'Widget restored to revision from %s', wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+            6  => 'Widget published.',
+            7  => 'Widget saved.',
+            8  => 'Widget submitted.',
+            9  => sprintf('Widget scheduled for: <strong>%1$s</strong>.', date_i18n( 'M j, Y @ G:i', strtotime( $post->post_date ) )),
+            10 => 'Widget draft updated.'
+        );
+        if ( $post_type_object->publicly_queryable ) {
+            $permalink = get_permalink( $post->ID );
+            $view_link = sprintf( ' <a href="%s">%s</a>', esc_url( $permalink ), 'View Widget' );
+            $messages[ $post_type ][1] .= $view_link;
+            $messages[ $post_type ][6] .= $view_link;
+            $messages[ $post_type ][9] .= $view_link;
+            $preview_permalink = add_query_arg( 'preview', 'true', $permalink );
+            $preview_link = sprintf( ' <a target="_blank" href="%s">%s</a>', esc_url( $preview_permalink ), 'Preview Widget' );
+            $messages[ $post_type ][8]  .= $preview_link;
+            $messages[ $post_type ][10] .= $preview_link;
+        }
+        return $messages;
+    }
+    add_filter( 'post_updated_messages', 'widget_updated_messages' );
 
 
 /*  ==========================================================================
-     INTERACTION MESSAGES
+     TAXONOMIES
     ==========================================================================  */
 
-function widget_updated_messages( $messages ) {
-    global $post;
-
-    $permalink = get_permalink( $post );
-
-    $messages['widget'] = array(
-        0 => '', // Unused. Messages start at index 1.
-        1 => sprintf( __('Widget updated. <a target="_blank" href="%s">View Widget</a>', 'YOUR-TEXTDOMAIN'), esc_url( $permalink ) ),
-        2 => __('Custom field updated.', 'YOUR-TEXTDOMAIN'),
-        3 => __('Custom field deleted.', 'YOUR-TEXTDOMAIN'),
-        4 => __('Widget updated.', 'YOUR-TEXTDOMAIN'),
-        /* translators: %s: date and time of the revision */
-        5 => isset($_GET['revision']) ? sprintf( __('Widget restored to revision from %s', 'YOUR-TEXTDOMAIN'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
-        6 => sprintf( __('Widget published. <a href="%s">View Widget</a>', 'YOUR-TEXTDOMAIN'), esc_url( $permalink ) ),
-        7 => __('Widget saved.', 'YOUR-TEXTDOMAIN'),
-        8 => sprintf( __('Widget submitted. <a target="_blank" href="%s">Preview Widget</a>', 'YOUR-TEXTDOMAIN'), esc_url( add_query_arg( 'preview', 'true', $permalink ) ) ),
-        9 => sprintf( __('Widget scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview Widget</a>', 'YOUR-TEXTDOMAIN'),
-        // translators: Publish box date format, see http://php.net/date
-        date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( $permalink ) ),
-        10 => sprintf( __('Widget draft updated. <a target="_blank" href="%s">Preview Widget</a>', 'YOUR-TEXTDOMAIN'), esc_url( add_query_arg( 'preview', 'true', $permalink ) ) ),
-    );
-
-    return $messages;
-}
-add_filter( 'post_updated_messages', 'widget_updated_messages' );
-
-
-/*  ==========================================================================
-     TAXONOMY
-    ==========================================================================  */
-
-function types_init() {
-    register_taxonomy( 'types', array( 'widget' ), array(
-        'hierarchical'      => true,
-        'public'            => true,
-        'show_in_nav_menus' => true,
-        'show_ui'           => true,
-        'show_admin_column' => false,
-        'query_var'         => true,
-        'rewrite'           => true,
-        'capabilities'      => array(
-            'manage_terms'  => 'edit_posts',
-            'edit_terms'    => 'edit_posts',
-            'delete_terms'  => 'edit_posts',
-            'assign_terms'  => 'edit_posts'
-        ),
-        'labels'            => array(
-            'name'                       => __( 'Types', 'YOUR-TEXTDOMAIN' ),
-            'singular_name'              => _x( 'Types', 'taxonomy general name', 'YOUR-TEXTDOMAIN' ),
-            'search_items'               => __( 'Search Types', 'YOUR-TEXTDOMAIN' ),
-            'popular_items'              => __( 'Popular Types', 'YOUR-TEXTDOMAIN' ),
-            'all_items'                  => __( 'All Types', 'YOUR-TEXTDOMAIN' ),
-            'parent_item'                => __( 'Parent Types', 'YOUR-TEXTDOMAIN' ),
-            'parent_item_colon'          => __( 'Parent Types:', 'YOUR-TEXTDOMAIN' ),
-            'edit_item'                  => __( 'Edit Types', 'YOUR-TEXTDOMAIN' ),
-            'update_item'                => __( 'Update Types', 'YOUR-TEXTDOMAIN' ),
-            'add_new_item'               => __( 'New Types', 'YOUR-TEXTDOMAIN' ),
-            'new_item_name'              => __( 'New Types', 'YOUR-TEXTDOMAIN' ),
-            'separate_items_with_commas' => __( 'Types separated by comma', 'YOUR-TEXTDOMAIN' ),
-            'add_or_remove_items'        => __( 'Add or remove Types', 'YOUR-TEXTDOMAIN' ),
-            'choose_from_most_used'      => __( 'Choose from the most used Types', 'YOUR-TEXTDOMAIN' ),
-            'menu_name'                  => __( 'Types', 'YOUR-TEXTDOMAIN' ),
-        ),
-    ) );
-
-}
-add_action( 'init', 'types_init' );
+    function widget_taxonomies() {
+        $labels = array(
+            'name'              => 'Types', 'taxonomy general name',
+            'singular_name'     => 'Type', 'taxonomy singular name',
+            'search_items'      => 'Search Types',
+            'all_items'         => 'All Types',
+            'parent_item'       => 'Parent Type',
+            'parent_item_colon' => 'Parent Type:',
+            'edit_item'         => 'Edit Type',
+            'update_item'       => 'Update Type',
+            'add_new_item'      => 'Add New Type',
+            'new_item_name'     => 'New Type Name',
+            'menu_name'         => 'Type',
+        );
+        $args = array(
+            'hierarchical'      => true,
+            'labels'            => $labels,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'query_var'         => true,
+            // 'rewrite'           => array( 'slug' => 'new-slug' ),
+        );
+        register_taxonomy( 'type', array( 'widget' ), $args );
+    }
+    add_action( 'init', 'widget_taxonomies', 0 );
