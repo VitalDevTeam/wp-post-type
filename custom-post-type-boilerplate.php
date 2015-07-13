52ae2191903f4ed5f5755bae7929e6837e78b5ad
Adam Walter <adam@vtldesign.com>
2015-07-13 16:43:32 -0400

    Revert "Fix menu_position argument in register_post_type"
    This reverts commit d01a27af93fe0603a342d7f90a74a4ee6471caef.
    
    # Conflicts:
    #	custom-post-type-boilerplate.php

--- custom-post-type-boilerplate.php
+++ custom-post-type-boilerplate.php
@@ -1,152 +1,120 @@
 <?php
-
 /*
-	Plugin Name: Post Type (Description)
-	Plugin URI: https://github.com/VitalDevTeam/custom-post-type-boilerplate
-	Description: A custom post type for XXXXXX
-	Version: 1.0
-	Author: Vital Dev Team
-	Author URI: http://vtldesign.com
-	License: GPLv2
- */
+    Plugin Name: Post Type (Description)
+    Plugin URI: https://github.com/VitalDevTeam/custom-post-type-boilerplate
+    Description: A custom post type for _____
+    Version: 1.0
+    Author: Vital Dev Team
+    Author URI: http://vtldesign.com
+    License: GPL2
+*/
 
-// Exit if accessed directly
-if (! defined('ABSPATH')) exit;
+/*  ==========================================================================
+     INIT
+    ==========================================================================  */
 
-class WP_Custom_Post_Type_Boilerplate {
+    function widget_init() {
+        $labels = array(
+            'name'                => 'Widgets', 'post type general name',
+            'singular_name'       => 'Widget', 'post type singular name',
+            'menu_name'           => 'Widgets', 'admin menu',
+            'name_admin_bar'      => 'Widget', 'add new on admin bar',
+            'add_new'             => 'Add New', 'Widget',
+            'add_new_item'        => 'Add New Widget',
+            'new_item'            => 'New Widget',
+            'edit_item'           => 'Edit Widget',
+            'view_item'           => 'View Widget',
+            'all_items'           => 'All Widgets',
+            'search_items'        => 'Search Widgets',
+            'parent_item_colon'   => 'Parent Widgets:',
+            'not_found'           => 'No Widget found.',
+            'not_found_in_trash'  => 'No Widget found in Trash.'
+        );
+        $args = array(
+            'labels'              => $labels,
+            'public'              => true,
+            'publicly_queryable'  => true,
+            'show_ui'             => true,
+            'menu_icon'           => 'dashicons-star-filled', // Dashicons CSS class name - https://developer.wordpress.org/resource/dashicons/
+            'menu_position'       => 20,
+            'show_in_menu'        => true,
+            'query_var'           => true,
+            // 'rewrite'            => array( 'slug' => 'widget' ),
+            'capability_type'     => 'post',
+            'has_archive'         => true,
+            'hierarchical'        => false,
+            'menu_position'       => null,
+            'supports'            => array( 'title', 'editor' ),
+            'exclude_from_search' => false
+        );
+        register_post_type( 'widget', $args );
+    }
+    add_action( 'init', 'widget_init' );
 
-	private static $instance = null;
 
-	/**
-	 * Creates or returns an instance of this class
-	 */
-	public static function get_instance() {
+    /*  ==========================================================================
+         UPDATE MESSAGES
+        ==========================================================================  */
 
-		if ( null == self::$instance ) {
-			self::$instance = new self;
-		}
+    function widget_updated_messages( $messages ) {
+        $post             = get_post();
+        $post_type        = get_post_type( $post );
+        $post_type_object = get_post_type_object( $post_type );
+        $messages['widget'] = array(
+            0  => '',
+            1  => 'Widget updated.',
+            2  => 'Custom field updated.',
+            3  => 'Custom field deleted.',
+            4  => 'Widget updated.',
+            5  => isset( $_GET['revision'] ) ? sprintf( 'Widget restored to revision from %s', wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+            6  => 'Widget published.',
+            7  => 'Widget saved.',
+            8  => 'Widget submitted.',
+            9  => sprintf('Widget scheduled for: <strong>%1$s</strong>.', date_i18n( 'M j, Y @ G:i', strtotime( $post->post_date ) )),
+            10 => 'Widget draft updated.'
+        );
+        if ( $post_type_object->publicly_queryable & $post->post_type == 'widget' ) {
+            $permalink = get_permalink( $post->ID );
+            $view_link = sprintf( ' <a href="%s">%s</a>', esc_url( $permalink ), 'View Widget' );
+            $messages[ $post_type ][1] .= $view_link;
+            $messages[ $post_type ][6] .= $view_link;
+            $messages[ $post_type ][9] .= $view_link;
+            $preview_permalink = add_query_arg( 'preview', 'true', $permalink );
+            $preview_link = sprintf( ' <a target="_blank" href="%s">%s</a>', esc_url( $preview_permalink ), 'Preview Widget' );
+            $messages[ $post_type ][8]  .= $preview_link;
+            $messages[ $post_type ][10] .= $preview_link;
+        }
+        return $messages;
+    }
+    add_filter( 'post_updated_messages', 'widget_updated_messages' );
 
-		return self::$instance;
-	}
 
-	/**
-	 * Initialize plugin
-	 */
-	private function __construct() {
+/*  ==========================================================================
+     TAXONOMIES
+    ==========================================================================  */
 
-		// Load post type and taxonomies
-		add_action( 'init', array( $this, 'vital_widget_init' ), 0 );
-		add_action( 'init', array( $this, 'vital_widget_tax_init' ), 0 );
-
-		register_activation_hook( __FILE__, array( $this, 'activation' ) );
-		register_deactivation_hook( __FILE__, array( $this, 'deactivation' ) );
-
-	}
-
-    /**
-     * Plugin activation
-     */
-    public function activation() {
-		// Load post type and taxonomy
-		$this->vital_widget_init();
-		$this->vital_widget_tax_init();
-
-		// Flush permalinks
-		flush_rewrite_rules();
-	}
-
-    /**
-     * Plugin deactivation
-     */
-    public function deactivation() {
-		// Flush permalinks
-		flush_rewrite_rules();
-	}
-
-	/**
-	 * Register post type
-	 */
-	function vital_widget_init() {
-
-		$labels = array(
-			'name'                => _x( 'Widgets', 'Post Type General Name', 'vital' ),
-			'singular_name'       => _x( 'Widget', 'Post Type Singular Name', 'vital' ),
-			'menu_name'           => __( 'Widgets', 'vital' ),
-			'name_admin_bar'      => __( 'Widget', 'vital' ),
-			'parent_item_colon'   => __( 'Parent Widget:', 'vital' ),
-			'all_items'           => __( 'All Widgets', 'vital' ),
-			'add_new_item'        => __( 'Add New Widget', 'vital' ),
-			'add_new'             => __( 'Add New', 'vital' ),
-			'new_item'            => __( 'New Widget', 'vital' ),
-			'edit_item'           => __( 'Edit Widget', 'vital' ),
-			'update_item'         => __( 'Update Widget', 'vital' ),
-			'view_item'           => __( 'View Widget', 'vital' ),
-			'search_items'        => __( 'Search Widgets', 'vital' ),
-			'not_found'           => __( 'Not found', 'vital' ),
-			'not_found_in_trash'  => __( 'Not found in Trash', 'vital' ),
-		);
-		$args = array(
-			'label'               => __( 'vital_widget', 'vital' ),
-			'description'         => __( 'Widget', 'vital' ),
-			'labels'              => $labels,
-			'supports'            => array( 'title', ),
-	        'taxonomies'          => array( 'vital_widget_tax' ),
-			'hierarchical'        => false,
-			'public'              => true,
-			'show_ui'             => true,
-			'show_in_menu'        => true,
-			'menu_position'       => 20,
-			'menu_icon'           => 'dashicons-star-filled',
-			'show_in_admin_bar'   => true,
-			'show_in_nav_menus'   => false,
-			'can_export'          => true,
-			'has_archive'         => false,
-			'exclude_from_search' => true,
-			'publicly_queryable'  => true,
-			'capability_type'     => 'page',
-		);
-
-		register_post_type( 'vital_widget', $args );
-	}
-
-	/**
-	 * Register taxonomy
-	 */
-	function vital_widget_tax_init() {
-
-		$labels = array(
-			'name'                       => _x( 'Widget Types', 'Taxonomy General Name', 'vital' ),
-			'singular_name'              => _x( 'Widget Type', 'Taxonomy Singular Name', 'vital' ),
-			'menu_name'                  => __( 'Types', 'vital' ),
-			'all_items'                  => __( 'All Widget Types', 'vital' ),
-			'parent_item'                => __( 'Parent Widget Type', 'vital' ),
-			'parent_item_colon'          => __( 'Parent Widget Type:', 'vital' ),
-			'new_item_name'              => __( 'New Widget Type Name', 'vital' ),
-			'add_new_item'               => __( 'Add New Widget Type', 'vital' ),
-			'edit_item'                  => __( 'Edit Widget Type', 'vital' ),
-			'update_item'                => __( 'Update Widget Type', 'vital' ),
-			'view_item'                  => __( 'View Widget Type', 'vital' ),
-			'separate_items_with_commas' => __( 'Separate types with commas', 'vital' ),
-			'add_or_remove_items'        => __( 'Add or remove types', 'vital' ),
-			'choose_from_most_used'      => __( 'Choose from the most used', 'vital' ),
-			'popular_items'              => __( 'Popular Widget Types', 'vital' ),
-			'search_items'               => __( 'Search Widget Types', 'vital' ),
-			'not_found'                  => __( 'Not Found', 'vital' ),
-		);
-
-		$args = array(
-			'labels'                     => $labels,
-			'hierarchical'               => true,
-			'public'                     => true,
-			'show_ui'                    => true,
-			'show_admin_column'          => true,
-			'show_in_nav_menus'          => false,
-			'show_tagcloud'              => false,
-		);
-
-		register_taxonomy( 'vital_widget_tax', array( 'vital_widget' ), $args );
-	}
-
-}
-
-WP_Custom_Post_Type_Boilerplate::get_instance();
+    function widget_taxonomies() {
+        $labels = array(
+            'name'              => 'Types', 'taxonomy general name',
+            'singular_name'     => 'Type', 'taxonomy singular name',
+            'search_items'      => 'Search Types',
+            'all_items'         => 'All Types',
+            'parent_item'       => 'Parent Type',
+            'parent_item_colon' => 'Parent Type:',
+            'edit_item'         => 'Edit Type',
+            'update_item'       => 'Update Type',
+            'add_new_item'      => 'Add New Type',
+            'new_item_name'     => 'New Type Name',
+            'menu_name'         => 'Type',
+        );
+        $args = array(
+            'hierarchical'      => true,
+            'labels'            => $labels,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'query_var'         => true,
+            // 'rewrite'           => array( 'slug' => 'new-slug' ),
+        );
+        register_taxonomy( 'type', array( 'widget' ), $args );
+    }
+    add_action( 'init', 'widget_taxonomies', 0 );
