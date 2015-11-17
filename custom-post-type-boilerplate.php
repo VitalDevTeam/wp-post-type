b1499b25a89d5e496f04a2d62752ff622f8b91fb
Adam Walter <adam@adamwalter.com>
2015-11-17 12:24:16 -0500

    Add Tax_CTP_Filter class. Expand and revise example functions.
    Add vtl_widget_hide_date_dropdown() and vtl_widget_default_title() as optional.

--- custom-post-type-boilerplate.php
+++ custom-post-type-boilerplate.php
@@ -2,8 +2,8 @@
 /*
     Plugin Name: Post Type (Description)
     Plugin URI: https://github.com/VitalDevTeam/custom-post-type-boilerplate
-    Description: A custom post type for _____
-    Version: 1.1
+    Description: A custom post type for {description}
+    Version: 2.0
     Author: Vital Dev Team
     Author URI: http://vtldesign.com
     License: GPL2
@@ -14,81 +14,111 @@ if (! defined('ABSPATH')) exit;
 
 /**
  * Register Post Type
- * @return null
  */
-function widget_init() {
+function vtl_widget_init() {
 
     $labels = array(
-        'name'                   => _x( 'Widgets', 'Post Type General Name', 'vital' ),
-        'singular_name'          => _x( 'Widget', 'Post Type Singular Name', 'vital' ),
-        'menu_name'              => __( 'Widget', 'vital' ),
-        'name_admin_bar'         => __( 'Widget', 'vital' ),
-        'parent_item_colon'      => __( 'Parent Widget:', 'vital' ),
-        'all_items'              => __( 'All Widgets', 'vital' ),
-        'add_new_item'           => __( 'Add New Widget', 'vital' ),
-        'add_new'                => __( 'Add New', 'vital' ),
-        'new_item'               => __( 'New Widget', 'vital' ),
-        'edit_item'              => __( 'Edit Widget', 'vital' ),
-        'update_item'            => __( 'Update Widget', 'vital' ),
-        'view_item'              => __( 'View Widget', 'vital' ),
-        'search_items'           => __( 'Search Widgets', 'vital' ),
-        'not_found'              => __( 'Not found', 'vital' ),
-        'not_found_in_trash'     => __( 'Not found in Trash', 'vital' ),
+        'name'                  => 'Widgets',
+        'singular_name'         => 'Widget',
+        'menu_name'             => 'Widgets',
+        'name_admin_bar'        => 'Widget',
+        'parent_item_colon'     => 'Parent Widget:',
+        'all_items'             => 'All Widgets',
+        'add_new_item'          => 'Add New Widget',
+        'add_new'               => 'Add New',
+        'new_item'              => 'New Widget',
+        'edit_item'             => 'Edit Widget',
+        'update_item'           => 'Update Widget',
+        'view_item'             => 'View Widget',
+        'search_items'          => 'Search Widget',
+        'not_found'             => 'Not found',
+        'not_found_in_trash'    => 'Not found in Trash',
+        'items_list'            => 'Widgets list',
+        'items_list_navigation' => 'Widgets list navigation',
+        'filter_items_list'     => 'Filter widgets list',
     );
-    // $capabilities = array(
-    //     'create_posts'           => 'create_widgets',
-    //     'edit_post'              => 'edit_widget',
-    //     'read_post'              => 'read_widget',
-    //     'delete_post'            => 'delete_widget',
-    //     'delete_posts'           => 'delete_widgets',
-    //     'delete_private_posts'   => 'delete_private_widgets',
-    //     'delete_published_posts' => 'delete_published_widgets',
-    //     'delete_others_posts'    => 'delete_others_widgets',
-    //     'edit_posts'             => 'edit_widgets',
-    //     'edit_private_posts'     => 'edit_private_widgets',
-    //     'edit_others_posts'      => 'edit_others_widgets',
-    //     'edit_published_posts'   => 'edit_published_widgets',
-    //     'publish_posts'          => 'publish_widgets',
-    //     'read_private_posts'     => 'read_private_widgets',
-    // );
-    // $rewrite = array(
-    //     'slug'                   => 'custom-slug',
-    //     'with_front'             => true,
-    //     'pages'                  => true,
-    //     'feeds'                  => false,
-    // );
+    $rewrite = array(
+        'slug'                  => 'widget',
+        'with_front'            => true,
+        'pages'                 => true,
+        'feeds'                 => true,
+    );
     $args = array(
-        'label'                  => __( 'widget', 'vital' ),
-        'description'            => __( 'Widgets', 'vital' ),
-        'labels'                 => $labels,
-        'supports'               => array( 'title' ),
-        // 'taxonomies'             => array( 'taxonomy_one', 'taxonomy_two' ),
-        'hierarchical'           => false,
-        'public'                 => true,
-        'show_ui'                => true,
-        'show_in_menu'           => true,
-        'menu_position'          => 20,
-        'menu_icon'              => 'dashicons-star-filled', // https://developer.wordpress.org/resource/dashicons
-        'show_in_admin_bar'      => true,
-        'show_in_nav_menus'      => true,
-        'can_export'             => true,
-        'has_archive'            => true,
-        'exclude_from_search'    => false,
-        'publicly_queryable'     => true,
-        // 'rewrite'                => $rewrite,
-        'capability_type'        => 'post',
-        // 'capabilities'        => $capabilities,
-        // 'map_meta_cap'           => true,
+        'label'                 => 'Widget',
+        'description'           => 'Widget',
+        'labels'                => $labels,
+        'supports'              => array('title', 'editor', 'thumbnail'),
+        'taxonomies'            => array('widget_type'),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 5,
+        'menu_icon'             => 'dashicons-star-filled',
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'rewrite'               => $rewrite,
+        'capability_type'       => 'post',
     );
-
-    register_post_type( 'widget', $args );
+    register_post_type('widget', $args);
 
 }
+add_action('init', 'vtl_widget_init', 0);
+
+
+/**
+ * Register Taxonomy
+ */
+function vtl_widget_type_init() {
+
+    $labels = array(
+        'name'                       => 'Types',
+        'singular_name'              => 'Type',
+        'menu_name'                  => 'Types',
+        'all_items'                  => 'All Types',
+        'parent_item'                => 'Parent Type',
+        'parent_item_colon'          => 'Parent Type:',
+        'new_item_name'              => 'New Type Name',
+        'add_new_item'               => 'Add New Type',
+        'edit_item'                  => 'Edit Type',
+        'update_item'                => 'Update Type',
+        'view_item'                  => 'View Type',
+        'separate_items_with_commas' => 'Separate types with commas',
+        'add_or_remove_items'        => 'Add or remove types',
+        'choose_from_most_used'      => 'Choose from the most used',
+        'popular_items'              => 'Popular Types',
+        'search_items'               => 'Search Types',
+        'not_found'                  => 'Not Found',
+        'items_list'                 => 'Types list',
+        'items_list_navigation'      => 'Types list navigation',
+    );
+    $rewrite = array(
+        'slug'                       => 'widget-type',
+        'with_front'                 => true,
+        'hierarchical'               => false,
+    );
+    $args = array(
+        'labels'                     => $labels,
+        'hierarchical'               => true,
+        'public'                     => true,
+        'show_ui'                    => true,
+        'show_admin_column'          => true,
+        'show_in_nav_menus'          => true,
+        'show_tagcloud'              => true,
+        'rewrite'                    => $rewrite,
+    );
+    register_taxonomy('widget_type', array('widget'), $args);
+
+}
+add_action('init', 'vtl_widget_type_init', 0);
+
 
 /**
  * Post Type Update Messages
- * @param  object $messages Default messages
- * @return object           Customized messages
  */
 function widget_updated_messages( $messages ) {
 
@@ -125,48 +155,113 @@ function widget_updated_messages( $messages ) {
     return $messages;
 
 }
+add_filter('post_updated_messages', 'widget_updated_messages');
 
 
 /**
- * Register Taxonomy
- * @return null
+ * Customize title placeholder text
+ * Un-comment add_filter to enable
  */
-function widget_type_taxonomy() {
-
-    $labels = array(
-        'name'                       => _x( 'Types', 'Taxonomy General Name', 'vital' ),
-        'singular_name'              => _x( 'Type', 'Taxonomy Singular Name', 'vital' ),
-        'menu_name'                  => __( 'Types', 'vital' ),
-        'all_items'                  => __( 'All Types', 'vital' ),
-        'parent_item'                => __( 'Parent Type', 'vital' ),
-        'parent_item_colon'          => __( 'Parent Type:', 'vital' ),
-        'new_item_name'              => __( 'New Type Name', 'vital' ),
-        'add_new_item'               => __( 'Add New Type', 'vital' ),
-        'edit_item'                  => __( 'Edit Type', 'vital' ),
-        'update_item'                => __( 'Update Type', 'vital' ),
-        'view_item'                  => __( 'View Type', 'vital' ),
-        'separate_items_with_commas' => __( 'Separate types with commas', 'vital' ),
-        'add_or_remove_items'        => __( 'Add or remove types', 'vital' ),
-        'choose_from_most_used'      => __( 'Choose from the most used', 'vital' ),
-        'popular_items'              => __( 'Popular Types', 'vital' ),
-        'search_items'               => __( 'Search Types', 'vital' ),
-        'not_found'                  => __( 'Not Found', 'vital' ),
-    );
-
-    $args = array(
-        'labels'                     => $labels,
-        'hierarchical'               => true,
-        'public'                     => true,
-        'show_ui'                    => true,
-        'show_admin_column'          => true,
-        'show_in_nav_menus'          => true,
-        'show_tagcloud'              => false,
-    );
-
-    register_taxonomy( 'type', array( 'widget' ), $args );
-
+function vtl_widget_default_title($title) {
+     $screen = get_current_screen();
+     if  ('widget' === $screen->post_type) {
+          $title = 'Enter widget name here';
+     }
+     return $title;
 }
+// add_filter('enter_title_here', 'vtl_widget_default_title');
 
-add_filter( 'post_updated_messages', 'widget_updated_messages' );
-add_action( 'init', 'widget_init' );
-add_action( 'init', 'widget_type_taxonomy', 0 );
+/**
+ * Hide date filter in post list
+ * Un-comment add_filter to enable
+ */
+function vtl_widget_hide_date_dropdown($months) {
+    $screen = get_current_screen();
+    if  ('widget' === $screen->post_type) {
+        return array();
+    } else {
+        return $months;
+    }
+}
+// add_filter('months_dropdown_results', 'vtl_widget_hide_date_dropdown');
+
+
+/**
+* Tax CTP Filter Class
+* Simple class to add custom taxonomy dropdown to a custom post type admin edit list
+* @author Ohad Raz <admin@bainternet.info>
+* @version 0.1
+*/
+if (!class_exists('Tax_CTP_Filter')) {
+    class Tax_CTP_Filter
+    {
+        /**
+         * __construct
+         * @author Ohad Raz <admin@bainternet.info>
+         * @since 0.1
+         * @param array $cpt [description]
+         */
+        function __construct($cpt = array()) {
+            $this->cpt = $cpt;
+            // Adding a Taxonomy Filter to Admin List for a Custom Post Type
+            add_action( 'restrict_manage_posts', array($this,'my_restrict_manage_posts' ));
+        }
+
+        /**
+         * my_restrict_manage_posts  add the slelect dropdown per taxonomy
+         * @author Ohad Raz <admin@bainternet.info>
+         * @since 0.1
+         * @return void
+         */
+        public function my_restrict_manage_posts() {
+            // only display these taxonomy filters on desired custom post_type listings
+            global $typenow;
+            $types = array_keys($this->cpt);
+            if (in_array($typenow, $types)) {
+                // create an array of taxonomy slugs you want to filter by - if you want to retrieve all taxonomies, could use get_taxonomies() to build the list
+                $filters = $this->cpt[$typenow];
+                foreach ($filters as $tax_slug) {
+                    // retrieve the taxonomy object
+                    $tax_obj = get_taxonomy($tax_slug);
+                    $tax_name = $tax_obj->labels->name;
+
+                    // output html for taxonomy dropdown filter
+                    echo "<select name='".strtolower($tax_slug)."' id='".strtolower($tax_slug)."' class='postform'>";
+                    echo "<option value=''>All $tax_name</option>";
+                    $this->generate_taxonomy_options($tax_slug,0,0,(isset($_GET[strtolower($tax_slug)])? $_GET[strtolower($tax_slug)] : null));
+                    echo "</select>";
+                }
+            }
+        }
+
+        /**
+         * generate_taxonomy_options generate dropdown
+         * @author Ohad Raz <admin@bainternet.info>
+         * @since 0.1
+         * @param  string  $tax_slug
+         * @param  string  $parent
+         * @param  integer $level
+         * @param  string  $selected
+         * @return void
+         */
+        public function generate_taxonomy_options($tax_slug, $parent = '', $level = 0,$selected = null) {
+            $args = array('show_empty' => 1);
+            if(!is_null($parent)) {
+                $args = array('parent' => $parent);
+            }
+            $terms = get_terms($tax_slug,$args);
+            $tab='';
+            for($i=0;$i<$level;$i++){
+                $tab.='--';
+            }
+
+            foreach ($terms as $term) {
+                // output each select option line, check against the last $_GET to show the current option selected
+                echo '<option value='. $term->slug, $selected == $term->slug ? ' selected="selected"' : '','>' .$tab. $term->name .' (' . $term->count .')</option>';
+                $this->generate_taxonomy_options($tax_slug, $term->term_id, $level+1,$selected);
+            }
+
+        }
+    }
+}
+new Tax_CTP_Filter(array('widget' => array('widget_type')));
