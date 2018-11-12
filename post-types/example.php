dbc1e4526f50b96ecb0bfb928f765612254f0da8
Adam Walter <adam@vtldesign.com>
2018-11-12 18:14:28 -0500

    Lints PHP in example.php

--- post-types/example.php
+++ post-types/example.php
@@ -3,29 +3,29 @@
  * Example post type
  */
 
- // Define our post type names
+// Define our post type names
 $widget_names = [
-    'name' => 'vtl_widget',
+	'name'      => 'vtl_widget',
 	'menu_name' => 'Widgets',
-    'singular' => 'Widget',
-    'plural' => 'Widgets',
+	'singular'  => 'Widget',
+	'plural'    => 'Widgets',
 	'all_items' => 'All Widgets',
-    'slug' => 'widget'
+	'slug'      => 'widget',
 ];
 
 // Define our options
 $widget_options = [
-    'exclude_from_search' => false,
-    'hierarchical'        => false,
-    'menu_position'       => 20,
-    'has_archive'         => true,
-    'rewrite'             => array('with_front' => false),
-    'show_in_admin_bar'   => true,
-    'show_in_menu'        => true,
-    'show_in_nav_menus'   => true,
-    'show_in_rest'        => false,
-    'show_ui'             => true,
-    'supports'            => array('title'),
+	'exclude_from_search' => false,
+	'hierarchical'        => false,
+	'menu_position'       => 20,
+	'has_archive'         => true,
+	'rewrite'             => array('with_front' => false),
+	'show_in_admin_bar'   => true,
+	'show_in_menu'        => true,
+	'show_in_nav_menus'   => true,
+	'show_in_rest'        => false,
+	'show_ui'             => true,
+	'supports'            => array('title'),
 ];
 
 // Create post type
@@ -50,12 +50,12 @@ $widget->columns()->set([
 
 // Add custom admin columns to default array
 $widget->columns()->add([
-    'widget_color' => 'Color'
+	'widget_color' => 'Color',
 ]);
 
 // Populate custom admin columns
 $widget->columns()->populate('widget_color', function($column, $post_id) {
-    echo get_post_meta($post_id, 'widget_color');
+	echo get_post_meta($post_id, 'widget_color');
 });
 
 // Add CSS to style columns
@@ -72,24 +72,24 @@ add_action('admin_head', function() {
 
 // Make custom admin columns sortable
 $widget->columns()->sortable([
-    'widget_color' => ['widget_color', true]
+	'widget_color' => ['widget_color', true]
 ]);
 
 // Define taxonomy names
 $widget_type_names = [
-    'name'     => 'widget_type',
-    'singular' => 'Widget Type',
-    'plural'   => 'Widget Types',
-    'slug'     => 'widget-type'
+	'name'     => 'widget_type',
+	'singular' => 'Widget Type',
+	'plural'   => 'Widget Types',
+	'slug'     => 'widget-type',
 ];
 
 // Define taxonomy options
 $widget_type_options = [
-    'heirarchical'      => true,
-    'labels'            => array('menu_name' => 'Types'),
-    'show_admin_column' => true,
-    'show_in_nav_menus' => false,
-    'show_in_rest'      => true
+	'heirarchical'      => true,
+	'labels'            => array('menu_name' => 'Types'),
+	'show_admin_column' => true,
+	'show_in_nav_menus' => false,
+	'show_in_rest'      => true,
 ];
 
 // Register taxonomy
